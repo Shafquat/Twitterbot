996b436fa28e3cc6cd7759cd7fa43b51d87960eb
Shafquat <shafquatarefeen@gmail.com>
2015-01-21 23:52:59 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TweetSharp;
+
+namespace Twitterbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pass your credentials to the service
+            TwitterService service = new TwitterService("App_ConsumerKey", "App_ConsumerSecret");
+            service.AuthenticateWith("accessToken", "tokenSecret");
+
+            SearchOptions options = new SearchOptions { Q = "indie pop", Resulttype = TwitterSearchResultType.Recent };
+            var searchedTweets = service.Search(options);
+
+            if (searchedTweets != null)
+            {
+                foreach (var tweet in searchedTweets.Statuses )
+                {
+                    if (!tweet.Text.Contains("RT"))
+                    {
+                        TwitterStatus s = service.SendTweet(new SendTweetOptions { Status = "RT: " + tweet.User.ScreenName + " " + tweet.Text });
+                    }
+
+                }
+            }
+
+            //shows most recent tweets in console
+            var tweets = service.ListTweetsOnHomeTimeline(new ListTweetsOnHomeTimelineOptions());
+            foreach (var tweeti in tweets)
+            {
+                Console.WriteLine("{0} says '{1}'", tweeti.User.ScreenName, tweeti.Text);
+            }
+            Console.WriteLine("Press Enter to exit");
+            Console.Read();
+
+        }
+
+
+    }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+In the code please enter your App_ConsumerKey, App_ConsumerSecret, accessToken, and tokenSecret
+Make sure your twitter app has writing permissions
+To change the hashtag, change the text "indie pop" to whatever you want
